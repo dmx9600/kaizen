b562a73438cc666798d12c67f44cac6109282ae7
DinukaWa <DinukaWa@INT-LT-ITD035.masgrp.corp.pri>
2015-03-04 12:34:30 +0530

    Autocomplete Factory

--- scripts/googlemap.js
+++
@@ -1,8 +0,0 @@
-'GoogleMapApiProvider'.ns() == 'uiGmapGoogleMapApiProvider'
-    .config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
-        GoogleMapApi.configure({
-            key: 'AIzaSyD681phGR_8CsmFlL4NinO5nyvkyiMUiGg',
-            v: '3.17',
-            libraries: 'weather,geometry,visualization'
-        });
-    }]);
